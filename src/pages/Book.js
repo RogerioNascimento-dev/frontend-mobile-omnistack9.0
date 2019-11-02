0e3326129ed05bf766fdff1beb9a2d59254327cd
RogerioNascimento-dev <rogerionascimento.dev@outlook.com.br>
2019-11-02 04:09:36 -0300

    já retornando o id da base de dados

---
+++ src/pages/Book.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text, StylesSheet} from 'react-native';
+
+export default function Book(){
+  return (
+    <View>
+      <Text>Book</Text>
+    </View>
+  );
+}
