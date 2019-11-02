df74287f6022ec0d5c8c270f61c3233e6bbd10f3
RogerioNascimento-dev <rogerionascimento.dev@outlook.com.br>
2019-11-02 14:09:19 -0300

    implementado e configurado exibição de nome do usuario logado e botão de sair

--- src/pages/List.js
+++ src/pages/List.js
@@ -2,6 +2,7 @@ import React,{ useState, useEffect } from 'react';
 import { SafeAreaView,Text,TouchableOpacity, ScrollView, Image, StyleSheet, AsyncStorage } from 'react-native';
 import logo from '../assets/logo.png';
 import SpotList from '../components/SpotList';
+import { Entypo } from '@expo/vector-icons/'
 
 export default function List({ navigation }){
   const [tecnologias, setTecnologias] = useState([]);
@@ -26,8 +27,9 @@ export default function List({ navigation }){
   return (
     <SafeAreaView style={styles.container}>
       <Image source={logo} style={styles.logo}/>
-      <TouchableOpacity onPress={()=> handleLogout()}>
+      <TouchableOpacity style={styles.contentUsuario} onPress={()=> handleLogout()}>
       <Text>{`Olá, ${user_name}`}</Text>
+      <Entypo name="log-out" size={12} color="red" />
       </TouchableOpacity>
 
       <ScrollView>
@@ -44,6 +46,15 @@ export default function List({ navigation }){
 const styles = StyleSheet.create({
   container: {
     flex: 1,   
+  },
+  contentUsuario:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  contentUsuarioText:{
+    fontSize: 12,
+    marginRight: 5,
   },
   logo:{
     height: 32,
